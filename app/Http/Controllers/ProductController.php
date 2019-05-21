2fd4db3904b27ca92bda74e3c193a2b92d87d9ac
heta <hetathakkar10@gmail.com>
2019-05-20 23:05:50 -0700

    product collection resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
 use App\Model\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+
     public function index()
     {
-       return Product::all();
+       return ProductCollection::collection(Product::all());
     }
 
     public function create()
