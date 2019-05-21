680a8bce80cfee20c3352d1b65ea1de889c2c619
heta <hetathakkar10@gmail.com>
2019-05-21 03:48:07 -0700

    passport installed

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
+//authentication
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
 
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function (
+    ) {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
 });
-Route::apiResource('/products','ProductController');
-Route::group(['prefix'=>'products'],function(){
-Route::apiResource('/{product}/reviews','ReviewController');
+//products and reviews
+Route::apiResource('/products', 'ProductController');
+Route::group(['prefix' => 'products'], function () {
+    Route::apiResource('/{product}/reviews', 'ReviewController');
+
+
 });
